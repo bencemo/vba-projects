d8385f5b9f10339897b7ec3141d562d83b7f8351
bencemo <donkajak@gmail.com>
2017-05-23 21:17:59 +0200

    BasicForCycle_Ready
    Ready

--- TestigForCycle.vb
+++ TestigForCycle.vb
@@ -1,9 +1,32 @@
-Sub TestingForCyclye
+Sub TestingForCyclye1()
 
 'Only for testing, practicing purposes
 
-Dim i as Integer
-Dim MyCellColumn As String
-Dim MyCell 
+'Ask for variants
 
+Dim i As Integer
+Dim UserInput As Variant
+Dim MyColumn As String
+Dim MyCell  As String
+
+'Clear all previous cells
+    Cells.Select
+    Range("D4").Activate
+    Selection.ClearContents
+
+'Define variants
+MyColumn = "A"
+
+'Ask for user's input
+UserInput = InputBox("How many cells?")
+
+'Run the for cycle
+For i = 1 To UserInput
+    MyCell = MyColumn & i
+    Range(MyCell).Select
+    ActiveCell = i
+    
+Next i
+
+'End the Sub
 End Sub
